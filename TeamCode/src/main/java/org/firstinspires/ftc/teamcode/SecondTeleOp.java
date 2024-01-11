3f2d9e13e627a676a2384f8b71f7842d5b55bd2b
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2024-01-11 07:16:56 -0500

    Created second TeleOp for new robot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SecondTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SecondTeleOp.java
@@ -1,4 +1,58 @@
 package org.firstinspires.ftc.teamcode;
+//Nebuala 12382
+//Simply two wheel tak drive
 
-public class SecondTeleOp {
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+//1. Annotate teleop
+//2. Extend the OpMode class and implement required methods
+@TeleOp(name="Tank Drive T Philip")
+public class SecondTeleOp extends OpMode {
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+
+    DcMotor armMotor;
+
+    public final int UP_POSITION = 0;
+    public final int DOWN_POSITION = 0;
+
+    @Override
+    public void init() {
+        //4. Hardware map the motors
+        leftMotor = hardwareMap.get(DcMotor.class,"leftMotor");
+        rightMotor = hardwareMap.get(DcMotor.class,"rightMotor");
+        armMotor = hardwareMap.get(DcMotor.class, "armMotor");
+
+
+        //5. Set the motors to the proper direction
+        rightMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+
+    @Override
+    public void loop() {
+        //6. Set power based on gamestick y values
+        leftMotor.setPower(-gamepad1.left_stick_y);
+        rightMotor.setPower(-gamepad1.right_stick_y);
+
+        if(gamepad1.dpad_up){
+            armMotor.setTargetPosition(UP_POSITION);
+        }
+        if(gamepad1.dpad_down){
+            armMotor.setTargetPosition(DOWN_POSITION);
+        }
+    }
 }
+
