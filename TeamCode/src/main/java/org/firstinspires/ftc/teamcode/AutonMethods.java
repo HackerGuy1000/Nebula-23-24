f38b2e72b53291bb415522c845c2ad55e13535a2
HackerGuy1000 <107278740+HackerGuy1000@users.noreply.github.com>
2024-01-11 07:18:00 -0500

    Refactored the Mecanum auton methods

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonMethods.java
@@ -11,11 +11,6 @@ public class AutonMethods {
     private DcMotor backLeft;
     private DcMotor backRight;
 
-    private final double CM = 537.7/ 6201;
-
-    public int toTicks(double value, double unit){
-        return (int)(unit * value);
-    }
     public void hardwareMap(HardwareMap hardwareMap){
         frontLeft = hardwareMap.get(DcMotor.class, "FL");
         frontRight = hardwareMap.get(DcMotor.class, "FR");
@@ -23,26 +18,21 @@ public class AutonMethods {
         backRight = hardwareMap.get(DcMotor.class, "BR");
 
     }
-    public void Drive(double x, double y){
-        int driveTicks = toTicks(x,CM);
-        int strafeTicks = toTicks(y,CM);
+    public void Drive(double theta, double power, double turn){
+        double sin = Math.sin(theta-Math.PI/4);
+        double cos = Math.cos(theta - Math.PI/4);
+        double max = Math.max(Math.abs(sin),Math.abs(cos));
 
-        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
-        frontLeft.setTargetPosition(driveTicks + strafeTicks);
-        frontRight.setTargetPosition(driveTicks - strafeTicks);
-        backLeft.setTargetPosition(driveTicks - strafeTicks);
-        backRight.setTargetPosition(driveTicks + strafeTicks);
-
-        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-
+        double fl_power = power * cos/max + turn;
+        double fr_power = power * sin/max - turn;
+        double bl_power = power * sin/max + turn;
+        double br_power = power * cos/max - turn;
+        if((power + Math.abs(turn))> 1){
+            fl_power /= power + turn;
+            fr_power /= power + turn;
+            bl_power /= power + turn;
+            br_power /= power + turn;
+        }
 
 
     }
